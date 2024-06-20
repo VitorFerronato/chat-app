a43ee67f8cc450f380085f9cd9331937632d3d6e
Vitor Eduardo <vitor.eduardo@predify.me>
2024-06-20 15:04:27 -0300

    feat: create user route

--- server/Controllers/userController.js
+++ server/Controllers/userController.js
@@ -1,7 +1,43 @@
 const userModel = require("../Models/userModel")
+const bcrypt = require("bcrypt")
+const validator = require("validator")
+const jwt = require("jsonwebtoken")
 
-const registerUser = (req, res) => {
-    res.send("Register")
+const createToken = (_id) => {
+    const jwtkey = process.env.JWT_SECRET_KEY
+
+    return jwt.sign({ _id }, jwtkey, { expiresIn: "3d" })
 }
+
+const registerUser = async (req, res) => {
+    try {
+        const { name, email, password } = req.body
+
+        let user = await userModel.findOne({ email })
+
+        // Validations
+        if (user) return res.status(400).json("User already exists")
+        if (!name || !email || !password) return res.status(400).json("All fields are required")
+        if (!validator.isEmail(email)) return res.status(400).json("Must be a valid email")
+        if (!validator.isStrongPassword(password)) return res.status(400).json("Must be a strong password")
+
+        user = new userModel({ name, email, password })
+
+        const salt = await bcrypt.genSalt(10)
+        user.password = await bcrypt.hash(user.password, salt)
+
+        await user.save()
+
+        const token = createToken(user._id)
+
+        res.status(200).json({ _id: user._id, name, email, token })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json(error)
+    }
+
+}
+
+
 
 module.exports = { registerUser }
