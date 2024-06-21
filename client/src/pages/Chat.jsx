25a0c91046b9817d9ba85875ae58cdd9a5401591
Vitor Eduardo <vitor.eduardo@predify.me>
2024-06-21 13:33:13 -0300

    feat: get new chats

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -3,12 +3,14 @@ import { AuthContext } from "../context/AuthContext"
 import { ChatContext } from "../context/ChatContext"
 import { Container, Stack } from "react-bootstrap"
 import UserChat from "../components/chat/UserChat"
+import PotentialChats from "../components/chat/PotentialChats";
 
 const Chat = () => {
     const { user } = useContext(AuthContext)
     const { userChats, isUserChatsLoading, userChatsError } = useContext(ChatContext)
 
     return <Container>
+        <PotentialChats />
         {userChats?.length < 1 ? null :
             <Stack direction="horizontal" gap={4} className="align-items-start">
 
